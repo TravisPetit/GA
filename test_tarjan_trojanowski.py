5ec2f5bfce1c94dfc3141ea44676fcc03a3bad85
TravisPetit <travis.riverapetit@stud.unibas.ch>
2018-11-20 12:06:08 +0100

    Changed actual as expected in tests

--- test_tarjan_trojanowski.py
+++ test_tarjan_trojanowski.py
@@ -10,81 +10,81 @@ class TestTarjanTrojanowski(unittest.TestCase):
         for i in range(3):
             V,E = generate_graph_of_degree(0)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree1(self):
         for i in range(3):
             V,E = generate_graph_of_degree(1)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree2(self):
         for i in range(3):
             V,E = generate_graph_of_degree(2)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree3(self):
         for i in range(3):
             V,E = generate_graph_of_degree(3)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree4(self):
         for i in range(3):
             V,E = generate_graph_of_degree(4)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree5(self):
         for i in range(3):
             V,E = generate_graph_of_degree(5)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree6(self):
         for i in range(3):
             V,E = generate_graph_of_degree(6)
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_degree7_and_higher(self):
         for i in range(3):
             V,E = generate_graph_of_degree(randint(7,25))
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
     def test_random_graph(self):
         for i in range(3):
             V,E = generate_random_graph()
             Ec = G_complement(V,E)
-            actual = len(naive(V, Ec))
-            expected = maxset(V,E)
-            self.assertEqual(actual, expected)
+            expected = len(naive(V, Ec))
+            actual = maxset(V,E)
+            self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
