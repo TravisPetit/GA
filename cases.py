4b11809d1461d1dbb4ba23e37bbc28b3771bd082
TravisPetit <travis.riverapetit@stud.unibas.ch>
2018-11-26 13:21:31 +0100

    Add statement 3.4.2.2 and 3.4.3

--- cases.py
+++ cases.py
@@ -1,4 +1,17 @@
 def case_3_4_2_1(A1c, A2c, A3c):
-    """ Returns True if |Aic & Ajc| <= |A1c & A2c & A3c| for all i!=j """
+    """ Returns True if |Aic & Ajc| <= |A1c & A2c & A3c| for all i!=j """
     temp = len(A1c & A2c & A3c) + 1
     return len(A1c & A2c) <= temp and len(A1c & A3c) <= temp and len(A2c & A3c) <= temp
+
+def case_3_4_2_2(A1c, A2c, A3c):
+    """ Returns the Aic, Ajc such that |A1c & A2c| > |A1c & A2c & A3c| + 2 """
+    temp = len(A1c & A2c & A3c) + 2
+    if len(A1c & A2c) > temp:
+        return A1c, A2c
+    if len(A1c & A3c) > temp:
+        return A1c, A3c
+    if len(A2c & A3c) > temp:
+        return A2c, A3c
+    raise Exception("Case 3.4.2.2: no such set.")
+
+case_3_4_2_2(set(),set(),set())
--- tarjan_trojanowski.py
+++ tarjan_trojanowski.py
@@ -111,3 +111,20 @@ def maxset(V, E):
                     temp2 = A1c & A2c & A3c
                     E2 = fun.induced(temp2, E)
                     return max(1 + maxset(temp1, E1), 3 + maxset(temp2, E2))
+
+    # -- STATEMENT 3.4.2.2 -- #
+                else:
+                    Aic, Ajc = cases.case_3_4_2_2(A1c, A2c, A3c)
+                    temp1 = V - {v,w1,w2,w3}
+                    E1 = fun.induced(temp1, E)
+                    temp2 = Aic & Ajc
+                    E2 = fun.induced(temp2, E)
+                    temp3 = A1c & A2c & A3c
+                    E3 = fun.induced(temp3, E)
+                    return max(1 + maxset(temp1, E1), 2 + maxset(temp2, E2), 3 + maxset(temp3, E3))
+
+    # -- STATEMENT 3.4.3 -- #
+            elif len(A1c & A2c & A3c) <= len(V) - 10:
+
+    # -- STATEMENT 3.4.3.3 -- #
+               pass
