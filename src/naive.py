3ad4d0654fd7c05b9df8d8a30c60e76883829a93
TravisPetit <travis.riverapetit@stud.unibas.ch>
2018-11-29 14:24:29 +0100

    Improve naive algorithm implementation

--- src/naive.py
+++ src/naive.py
@@ -2,9 +2,8 @@ import functions as fun
 
 def naive(V, E):
     """ Returns the biggest clique in V """
-    max_set = []
     pset = fun.powerset(V)
     for subset in pset:
-        if fun.is_clique(subset, E) and len(subset) > len(max_set):
-           max_set = subset
-    return max_set
+        if fun.is_clique(subset, E):
+            subset
+    return set()
