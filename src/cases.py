a7d67c8cb2a99734771a915b37998681f22c4914
TravisPetit <travis.riverapetit@stud.unibas.ch>
2018-12-01 17:14:06 +0100

    Fix cases spacing

--- src/cases.py
+++ src/cases.py
@@ -5,13 +5,16 @@ def case_3_4_2_1(A1c, A2c, A3c):
     temp = len(A1c & A2c & A3c) + 1
     return len(A1c & A2c) <= temp and len(A1c & A3c) <= temp and len(A2c & A3c) <= temp
 
+
 def case_3_4_2_2(A1c, A2c, A3c):
     """ Returns True if there is an Aic, Ajc : |Aic & Ajc| > |A1c & A2c & A3c| + 2 """
     return three_two_domination(A1c, A2c, A3c) is not None
 
+
 def case_3_4_3_1(A1c, A2c, A3c):
     """ Same as 3.4.2.1 """
     return case_3_4_2_1(A1c, A2c, A3c)
+
 
 def case_3_4_3_2(A1c, A2c, A3c):
     """ Same as 3.4.2.2 """
